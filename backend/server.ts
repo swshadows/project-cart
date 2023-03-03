e40175d7bcb80e8417acf5a23f195b6e9222ef66
Joe <joelson.jpl@gmail.com>
2023-03-03 11:52:18 -0400

    feat: Adicionando CORS

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,12 @@
 import express, { urlencoded } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 
 import routes from "./routes/routes";
 
 dotenv.config();
 const server = express();
+server.use(cors());
 server.use(urlencoded({ extended: false }));
 server.use(express.json());
 server.use(routes);
